e44a4f3a548dbc454d21e15c1260cee237c5cb03
durid-ah <ahmad.durid96@gmail.com>
2023-06-09 22:48:04 -0400

    hook up endpoint

--- app/main.go
+++ app/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	itemendpoints "github.com/durid-ah/item-tracker/app/item_endpoints"
+	userendpoints "github.com/durid-ah/item-tracker/app/user_endpoints"
+	"github.com/durid-ah/item-tracker/helpers"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func ConnectDatabase() *sql.DB {
+	db, err := sql.Open("sqlite3", "./something.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
 
 func main() {
-	fmt.Println("First run")
+	db := ConnectDatabase()
+	log.Println("Database connected...")
+
+	http.Handle("/signin", userendpoints.Signin(db))
+
+	http.Handle("/items", helpers.WithAuth(itemendpoints.GetItemsHandler(db)))
+
+	log.Println("Listening localhost:8080")
+	http.ListenAndServe("localhost:8080", nil)
 }
